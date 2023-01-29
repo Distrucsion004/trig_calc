7f9c7672adbaee06f4dcdb993286b14585da0bef
Distrucsion004 <razvan.curpan@gmail.com>
2023-01-29 02:34:01 +0200

    Works for all 4 basic trig functions

---
+++ script.py
@@ -0,0 +1,38 @@
+import math
+def deg_to_rad(d: int) -> float:
+    x = 180 / d
+    res = math.pi / x
+    return res
+def calc_sin(ang: float) -> float:
+    n = 1
+    t1 = ang**n/math.factorial(n)
+    
+    n = n+2
+    t2 = ang**n/math.factorial(n)
+    sum = 0 
+    while n<100:
+        sum = (sum + t1) - t2
+        n = n+2
+        t1 = ang**n/math.factorial(n)
+        n = n+2 
+        t2 = ang**n/math.factorial(n)
+    sum = sum + t2
+    sum = round(sum, 5)
+    return sum
+o = str(input("Please enter a trigonometric function (for a list of supported functions ennter ""help""): "))
+funcs = ["sin", "cos", "tg", "ctg"]
+a = -1
+while (True):
+    if o.strip() == "help":
+        print(funcs)
+    else:
+        for i in range(len(funcs)):
+            if o.strip() == funcs[i]:
+                a = i
+                break
+    if a != -1: break
+b = int(input("Please enter an angle (deg): "))
+fun = [calc_sin(deg_to_rad(b)), calc_sin(round(math.pi/2, 5) - deg_to_rad(b)), 
+calc_sin(deg_to_rad(b))/calc_sin(round(math.pi/2, 5)- deg_to_rad(b)), 1/calc_sin(deg_to_rad(b))/calc_sin(round(math.pi/2, 5)- deg_to_rad(b))]
+print(fun[a])
+
