51a300ab247783743b777f0bdf4f7ad8a5cb4c06
Distrucsion004 <razvan.curpan@gmail.com>
2023-01-29 13:40:06 +0200

    Fixed ctg formula and put everything in infinite loop

--- script.py
+++ script.py
@@ -10,7 +10,7 @@ def calc_sin(ang: float) -> float:
     n = n+2
     t2 = ang**n/math.factorial(n)
     sum = 0 
-    while n<100:
+    while n<110:
         sum = (sum + t1) - t2
         n = n+2
         t1 = ang**n/math.factorial(n)
@@ -22,17 +22,21 @@ def calc_sin(ang: float) -> float:
 o = str(input("Please enter a trigonometric function (for a list of supported functions ennter ""help""): "))
 funcs = ["sin", "cos", "tg", "ctg"]
 a = -1
+
 while (True):
+    o = str(input("Please enter a trigonometric function (for a list of supported functions ennter ""help""): "))
+    a = -1
     if o.strip() == "help":
         print(funcs)
+        
     else:
         for i in range(len(funcs)):
             if o.strip() == funcs[i]:
                 a = i
-                break
-    if a != -1: break
-b = int(input("Please enter an angle (deg): "))
-fun = [calc_sin(deg_to_rad(b)), calc_sin(round(math.pi/2, 5) - deg_to_rad(b)), 
-calc_sin(deg_to_rad(b))/calc_sin(round(math.pi/2, 5)- deg_to_rad(b)), 1/calc_sin(deg_to_rad(b))/calc_sin(round(math.pi/2, 5)- deg_to_rad(b))]
-print(fun[a])
+    if a != -1: 
+        b = int(input("Please enter an angle (deg): "))
+        fun = [calc_sin(deg_to_rad(b)), calc_sin(round(math.pi/2, 5) - deg_to_rad(b)), 
+        calc_sin(deg_to_rad(b))/calc_sin(round(math.pi/2, 5)- deg_to_rad(b)), 1/(calc_sin(deg_to_rad(b))/calc_sin(round(math.pi/2, 5)- deg_to_rad(b)))]
+        print(fun[a])
+    a = -1
 
